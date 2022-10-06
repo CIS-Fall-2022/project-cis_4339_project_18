05f95dae46db5236840d2fc680a93d06e7b8666c
pritakapoor <77901332+pritakapoor@users.noreply.github.com>
2022-10-05 23:06:46 -0500

    Add files via upload
    changed up the variables

--- backend/models/models.js
+++ backend/models/models.js
@@ -136,71 +136,276 @@ let healthDataSchema = new Schema({
 }, {
     collection: 'healthData'
 });
+
 //contact collection information
-let contactDataSchema = new Schema({
-    _id: {
-        type: String,
-        default: uuid.v1
-    },
-    cellphoneNumber: {
-        type: Number,
-        required: true
-    },
-    homephoneNumber: {
-        type: Number,
-        required: true
-    },
-    workphoneNumber: {
-        type: Number,
-        required: true
-    },
-    emailAddress: {
-        type: String,
-        required: true
-    },
-    alternativeEmail: {
-        type: String,
-        required: true
-    },
-    address1: {
-        type: String,
-        required: true
-    },
-    address2: {
-        type: String,
-        required: true
-    },
-    zipCode:{
-        type: String,
-        required: true
-    },
-    stateID: {
-        type: String,
-        required: true
-    },
-    countryID: {
-        type: String,
-        required: true
-    },
-    emergencyContact: {
-        type: String,
-        required: true
-    },
-    relationID: {
-        type: String,
-        required: true
-    },
-    lastUpdate: {
-        type: Date,
-        required: true
-    },
+    let contactDataSchema = new Schema({
+        _id: {
+            type: String,
+            default: uuid.v1
+        },
+        cellphoneNumber: {
+            type: Number,
+            required: true
+        },
+        homephoneNumber: {
+            type: Number,
+            required: true
+        },
+        workphoneNumber: {
+            type: Number,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        alternativeEmail: {
+            type: String,
+            required: true
+        },
+        address1: {
+            type: String,
+            required: true
+        },
+        address2: {
+            type: String,
+            required: true
+        },
+        zipCode:{
+            type: String,
+            required: true
+        },
+        stateID: {
+            type: String,
+            required: true
+        },
+        countryID: {
+            type: String,
+            required: true
+        },
+        emergencyContact: {
+            type: String,
+            required: true
+        },
+        relationID: {
+            type: String,
+            required: true
+        },
+        lastUpdate: {
+            type: Date,
+            required: true
+        },
 }, {
-collection: 'contactData'
+    collection: 'contactData'
 });
+
+// Client Collection
+    let clientSchema = new Schema({
+        _id: {
+            type: String,
+            default: uuid.v1
+        },
+        clientID: {
+            type: Number,
+            required: true
+        },
+        intakeFormID: {
+            type: Number,
+            required: true
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        birthday: {
+            type: String,
+            required: true
+        },
+        gender: {
+            type: String,
+            required: true
+        }
+    },
+        {
+            collection: 'clients'
+    });
+
+// Intake Form Collection
+    let intakeSchema = new Schema({
+        _id: {
+            type: String,
+            default: uuid.v1
+        },
+        formID: {
+            type: Number,
+            required: true
+        },
+        clientID: {
+            type: Number,
+            required: true
+        },
+        startDate: {
+            type: String,
+            required: true
+        },
+        endDate: {
+            type: String
+        },
+        isPrevious: {
+            type: Boolean
+        }
+    },
+        {
+            collection: 'intakes'
+    });
+
+// Services Collection
+    let serviceSchema = new Schema({
+        _id: {
+            type: String,
+            default: uuid.v1
+        },
+        serviceID: {
+            type: Number,
+            required: true
+        },
+        clientID: {
+            type: Number,
+            required: true
+        },
+        serviceName: {
+            type: String,
+            required: true
+        },
+        serviceDesc: {
+            type: String
+        },
+        organization: {
+            type: String,
+            required: true
+        }
+    },
+        {
+            collection: 'services'
+    });
+
+// Events Collection
+    let eventSchema = new Schema({
+        _id: {
+            type: String,
+            default: uuid.v1
+        },
+        eventID: {
+            type: Number,
+            required: true
+        },
+        serviceID: {
+            type: Number,
+            required: true
+        },
+        eventName: {
+            type: String,
+            required: true
+        },
+        endDate: {
+            type: String
+        },
+        eventDesc: {
+            type: String
+        }
+    },
+        {
+            collection: 'events'
+        });
+
+// Logs Collection
+    let logSchema = new Schema({
+        _id: {
+            type: String,
+            default: uuid.v1
+        },
+        clientID: {
+            type: Number,
+            required: true
+        },
+        serviceID: {
+            type: Number,
+            required: true
+        },
+        accessDate: {
+            type: Date,
+            required: true
+        },
+      accessTime:{
+         type: String,
+         required: true
+        }  
+    },
+        {
+            collection: 'logs'
+        });
+
+// Organizations Collection
+    let orgDataSchema = new Schema({
+        _id: { 
+            type: String, 
+            default: uuid.v1 
+        },
+        orgID: {
+            type: Number,
+            required: true
+        },
+        org_desc: {
+            type: String
+        },
+        org_contact: {
+            phoneNumber: [{
+                type: String
+            }],
+            email: [{
+                type: String
+            }] 
+        },
+        org_address: {
+            line1: {
+                type: String
+            },
+            line2: {
+                type: String
+            },
+            city: {
+                type: String
+            },
+            county: {
+                type: String
+            },
+            zip: {
+                type: Number
+            }
+        }
+    }, {
+        collection: 'orgData'
+    });
+
 // create models from mongoose schemas
 const primarydata = mongoose.model('primaryData', primaryDataSchema);
 const eventdata = mongoose.model('eventData', eventDataSchema);
 const healthdata = mongoose.model('healthData', healthDataSchema);
 const contactdata = mongoose.model('contactData', contactDataSchema);
+const clients = mongoose.model('client', clientSchema);
+const intakes = mongoose.model('intake', intakeSchema);
+const services = mongoose.model('service', serviceSchema);
+const events = mongoose.model('events', eventSchema);
+const logs = mongoose.model('logs', logSchema);
+const orgs = mongoose.model('orgData', orgDataSchema);
+
 // package the models in an object to export 
-module.exports = { primarydata, eventdata, healthdata, contactdata }
+module.exports = { primarydata, eventdata, healthdata, contactdata, clients, intakes, services, events, logs, orgs}
